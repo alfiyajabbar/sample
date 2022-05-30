ae6f49d0d066d905bb50405b701f9313d57f5263
server <server@user>
2022-05-30 18:11:06 +0530

    sample

---
+++ src/test/java/samplejenkin/Test/Sample13Test.java
@@ -0,0 +1,17 @@
+package samplejenkin.Test;
+
+import org.testng.annotations.Test;
+
+public class Sample13Test {
+	public class Sample11Test {
+		 
+		@Test
+		public void sample1()
+		{
+			System.out.println("sample1 running");
+		}
+
+	}
+
+
+}
