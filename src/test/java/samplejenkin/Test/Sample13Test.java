3df46441abdd6349c26f34700f5ef09449e6c8d8
server <server@user>
2022-05-31 09:36:48 +0530

    testcases changed

--- src/test/java/samplejenkin/Test/Sample13Test.java
+++ src/test/java/samplejenkin/Test/Sample13Test.java
@@ -3,15 +3,15 @@ package samplejenkin.Test;
 import org.testng.annotations.Test;
 
 public class Sample13Test {
-	public class Sample11Test {
+
 		 
 		@Test
-		public void sample1()
+		public void sample3()
 		{
-			System.out.println("sample1 running");
+			System.out.println("sample3 running");
 		}
 
 	}
 
 
-}
+
